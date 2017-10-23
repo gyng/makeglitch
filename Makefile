c473d0f4265c307565e8d19549d823ef3db63295
Ng Guoyou <fauxface+github@gmail.com>
2017-10-24 03:54:15 +0800

    Add text glitching option

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 glitchmake: index.template.html glitch.js
-	sed s/glitchTimeout\(\)/window\.requestAnimationFrame\(glitchRaf\)/ glitch.js > glitch.turbo.js.tmp
-	sed -e '/SCRIPT_SRC/{r glitch.js' -e 'd}' index.template.html > index.html.tmp
-	sed -e '/SCRIPT_TURBO_SRC/{r glitch.turbo.js.tmp' -e 'd}' index.html.tmp > index.html
+	sed s/_o/{}/ glitch.js > glitch.images.js.tmp
+	sed s/_o/{G_T:true}/ glitch.js > glitch.images.text.js.tmp
+	sed s/_o/{TURBO:true}/ glitch.js > glitch.turbo.images.js.tmp
+	sed s/_o/{G_T:true,TURBO:true}/ glitch.js > glitch.turbo.images.text.js.tmp
+	sed -e '/SCRIPT_IMG_SRC/{r glitch.images.js.tmp' -e 'd}' index.template.html > index.html.0.tmp
+	sed -e '/SCRIPT_IMG_TEXT_SRC/{r glitch.images.text.js.tmp' -e 'd}' index.html.0.tmp > index.html.1.tmp
+	sed -e '/SCRIPT_TURBO_IMG_SRC/{r glitch.turbo.images.js.tmp' -e 'd}' index.html.1.tmp > index.html.2.tmp
+	sed -e '/SCRIPT_TURBO_IMG_TEXT_SRC/{r glitch.turbo.images.text.js.tmp' -e 'd}' index.html.2.tmp > index.html
 
 clean:
 	rm index.html
--- glitch.js
+++ glitch.js
@@ -1,6 +1,23 @@
 javascript: (function() {
   window.__glitch_runs = window.__glitch_runs ? window.__glitch_runs + 1 : 1;
 
+  const MODES = {
+    STANDARD: 'STANDARD',
+    TURBO: 'TURBO'
+  };
+
+  const OPTIONS = Object.assign(
+    { G_T: false, G_I: true, TURBO: false },
+    _o || {}
+  );
+
+  const GLITCH_TEXT = OPTIONS.G_T;
+  const GLITCH_IMAGES = OPTIONS.G_I;
+  const MODE = OPTIONS.TURBO ? MODES.TURBO : MODES.STANDARD;
+  if (MODE === MODES.TURBO) {
+    window.__glitch_runs = 3;
+  }
+
   const transformTranspose = (header, input) => {
     const idx =
       header + Math.floor(Math.random() * (input.length - header - 1));
@@ -15,6 +32,19 @@ javascript: (function() {
     const idx = header + Math.floor(Math.random() * (input.length - header));
     input[idx] = by;
     return input;
+  };
+
+  const transformText = str => {
+    let bytes = new TextEncoder('utf-8').encode(str);
+    const errors = Math.random() * (0.05 * str.length);
+    for (let i = 0; i < errors; i += 1) {
+      bytes =
+        Math.random() > 0.5
+          ? transformSubstitute(0, bytes)
+          : transformTranspose(0, bytes);
+    }
+    const string = new TextDecoder('utf-8').decode(bytes);
+    return string;
   };
 
   const glitchImage = image => {
@@ -46,7 +76,10 @@ javascript: (function() {
             const errors = Math.random() * 10 * window.__glitch_runs;
             let corrupted = data.slice(0);
             for (let i = 0; i < errors; i++) {
-              corrupted = Math.random() > 0.5 ? transformTranspose(100, corrupted) : transformSubstitute(100, corrupted);
+              corrupted =
+                Math.random() > 0.5
+                  ? transformTranspose(100, corrupted)
+                  : transformSubstitute(100, corrupted);
             }
 
             const corruptBlob = new Blob([corrupted], { type: 'image/png' });
@@ -55,20 +88,23 @@ javascript: (function() {
             image.src = corruptSrc;
           };
 
-          const glitchRaf = () => {
-            window.__glitch_runs = 3;
+          const glitchImageRaf = () => {
             glitchOnce();
-            window.requestAnimationFrame(glitchRaf);
+            window.requestAnimationFrame(glitchImageRaf);
           };
 
-          const glitchTimeout = (max = 300) => {
+          const glitchImageTimeout = (max = 300) => {
             glitchOnce();
             window.setTimeout(() => {
-              glitchTimeout(max);
+              glitchImageTimeout(max);
             }, Math.random() * max / 2 + Math.random() * max / 2);
           };
 
-          glitchTimeout();
+          if (MODE === MODES.STANDARD) {
+            glitchImageTimeout();
+          } else if (MODE === MODES.TURBO) {
+            glitchImageRaf();
+          }
         };
         fileReader.readAsArrayBuffer(blob);
       }, 'image/jpeg');
@@ -77,10 +113,64 @@ javascript: (function() {
     }
   };
 
-  const images = document.querySelectorAll('img');
-  images.forEach(i => {
-    i.complete
-      ? glitchImage(i)
-      : (i.onload = event => glitchImage(event.target));
-  });
+  const glitchText = n => {
+    const original = n.textContent;
+    const glitchText = () => {
+      n.textContent = transformText(original);
+    };
+
+    const glitchTextTimeout = (max = 1000) => {
+      glitchText();
+      window.setTimeout(() => {
+        glitchTextTimeout(max);
+      }, Math.random() * max / 2 + Math.random() * max / 2);
+    };
+
+    const glitchTextRaf = () => {
+      glitchText();
+      window.requestAnimationFrame(glitchTextRaf);
+    };
+    glitchTextTimeout();
+  };
+
+  const textNodesUnder = el => {
+    let n;
+    const a = [];
+    const walk = document.createTreeWalker(
+      el,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+
+    while ((n = walk.nextNode())) {
+      a.push(n);
+    }
+
+    return a;
+  };
+
+  const setupGlitch = () => {
+    if (GLITCH_IMAGES) {
+      const images = document.querySelectorAll('img');
+      images.forEach(i => {
+        i.complete
+          ? glitchImage(i)
+          : (i.onload = event => glitchImage(event.target));
+      });
+    }
+
+    if (GLITCH_TEXT) {
+      const textNodes = textNodesUnder(document.body).filter(
+        n => n.textContent && n.textContent.length > 0 && n.textContent.trim()
+      );
+      textNodes.forEach(glitchText);
+    }
+  };
+
+  if (document.readyState === 'complete') {
+    setupGlitch();
+  } else {
+    document.body.addEventListener('load', setupGlitch);
+  }
 })();
